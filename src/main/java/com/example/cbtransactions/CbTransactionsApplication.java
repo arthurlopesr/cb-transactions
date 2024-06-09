ec25cd2a188543f847135d9513ec81192a43b14f
Arthur Lopes <arthurlopr12@gmail.com>
2024-06-09 16:48:36 -0300

    feat: initial commit

---
+++ src/main/java/com/example/cbtransactions/CbTransactionsApplication.java
@@ -0,0 +1,13 @@
+package com.example.cbtransactions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CbTransactionsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CbTransactionsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/cbtransactions/CbTransactionsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cbtransactions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CbTransactionsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
